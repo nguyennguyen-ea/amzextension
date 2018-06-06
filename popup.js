396146661a3dbaf3d1dd756352d7d914604965b2
Nguyen Nguyen <nguyen.nguyen@eastagile.com>
2018-06-06 23:14:51 +0700

    -pop up example

--- popup.js
+++ popup.js
@@ -1,6 +1,32 @@
-$(function(){
-  $('#name').keyup(function(){
-    console.log('enter here')
-    $('#greet').text('hello ' + $('#name').val())
-  })
-})
+
+$(function() {
+
+  chrome.storage.sync.get('total', function(result){
+    $('#spend').text(result.total);
+  });
+
+  $('#submitAmount').click(function(){
+    console.log('submitAmount click');
+
+    chrome.storage.sync.get('total', function(budget) {
+      var newTotal = 0;
+      if (budget.total) {
+        newTotal += parseInt(budget.total);
+      }
+      console.log('newTotal ' + newTotal);
+
+      var amount = $('#amount').val();
+      if (amount) {
+        newTotal += parseInt(amount);
+      }
+      console.log('newTotal ' + newTotal);
+
+      chrome.storage.sync.set({
+        'total': newTotal
+      });
+      $('#spend').text(newTotal);
+      $('#amount').val("");
+    });
+
+  });
+});

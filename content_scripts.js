62f7fb97547d0b7b608b89e2871838c6e0e99e69
Nguyen Nguyen <nguyen.nguyen@eastagile.com>
2018-07-22 20:40:15 +0700

    add fast apply button
    -add tree button to fast apply card

--- content_scripts.js
+++ content_scripts.js
@@ -1,20 +1,75 @@
+var listCard = ["58ZT-JDWTKW-PKYJ", "NDVJ-99RKTT-ESCB", "EQNJ-JCN28J-62VM", "Z77Y-8V5VBF-UAJP", "ZZ3Z-KVA2KB-TWLX", "68QL-56X4GU-XH4G", "UMUY-WBJ5BL-WSZN", "LP2K-9GT2FB-G2FN", "PVJH-VFNJEY-QYXX", "ESQA-ZX7NE5-HM7L", "C3U7-6HDW5G-XSCM", "VLUX-ZFHRWW-JQ8N", "XTRG-XWQEPZ-4ZV7", "U35G-PEHQMN-6HZ4", "T4LR-P2SQN5-4TCC", "VGPM-BRPLGV-AE3D", "VLXN-PRRVKP-2KSE", "VECY-JW7Y78-AG2Y", "KVHB-LYV46P-LVAW", "7J5Z-MYKDS7-4PYB", "FSJD-HAR5C7-LQPR", "RB7V-YT9YRB-6XKB", "BUU9-3XENQP-GEBT", "PF5Q-H4SKHP-LCZL", "DPRH-EGN3QV-GJ6P", "AUKP-T7BLUP-UMZU", "4557-N4NNXX-TBE4", "BUJZ-9J3NVV-FVY5", "6Y2N-WB3T6C-BGX2", "V8H4-2KJ4GN-S4A6", "LHYE-N6A6AW-XF4M", "VWAX-BB8TMQ-2KV4", "YJ5Q-R4JRYB-TFBZ", "UF3Q-TSNSX4-3X6W", "M7Y6-46TJ5X-KWCU", "Q2XS-736WR6-HWWM", "JRHL-9U7YUK-RGZG", "MNMU-VBQSYR-5ME6", "MQ8S-QVAUZ6-JU3Q", "2S5C-UWT9WN-DXQ9", "GM6H-LBWYWX-6GQ6", "J4UL-ZVCU27-RH4H", "2T4V-TYJ3ZC-LN2U", "LDZ4-LJD9VQ-B2WH", "KTJX-XNKNHB-9RJY", "HTR9-39HTC2-8A5D", "4AFX-J893U9-FQ4X", "34HB-BWMA9R-H4E4", "P974-GD8GCS-NYEY", "98CV-FH8SMY-NGAE", "CUVL-V4JGWW-QHQG", "ZQRN-GW859H-TCXM", "DLYE-8CX6MQ-5YBG", "73LS-2DS99C-NRFC", "UUMS-S3EK2L-43BP", "9ESQ-CNSX7M-FH54", "KBFZ-8JCQHU-VEWB", "G2US-VVGGJQ-HWYQ", "WUHA-LWFU8V-WKJB", "LBS6-XYU8PP-AXM2", "3H77-LX5LFW-KP8L", "Y98Z-TML9FQ-GSJ5", "264V-QCPK57-5QJB", "T2FG-ZAZEAN-JD57", "TF4Q-AXD447-35KQ", "TN8V-L3X6P5-6W7S", "GEF9-RZPCP9-LJYW", "QRJC-6Q6875-U2VM", "FCKJ-Z9QCYW-PXKN", "PGC6-NBPVKV-YEKU", "6A22-VC88TP-XCA7", "RYAU-6JHZY9-A64Z", "36QN-XDHR6Z-NJNY", "XL5V-W7EQ9G-VFM8", "8Q29-MMVVN2-WY2L", "JW92-8BE8VU-V4AH", "6X5Y-XYZ9K8-WHCU", "PCF2-J6GCPL-APDT", "MQ9D-5NYUHL-LLD6", "4UGH-SSUERH-GQ6W", "S2JD-RT5TJS-US7P", "UBVE-PRUVV9-JS2Z", "5DGH-4DPCDQ-JFUT", "84AA-SQSYLT-85Y3", "TLJM-HNKZLX-ZR5Z", "ZP3S-AB5EYJ-NJYD", "MRZT-KRJ573-DACT", "A2CA-EXQHFL-BV96", "5DAY-9VYNWD-JUP7", "A5EY-6946FG-6QE9", "NCDP-WCG6D4-W2YU", "NX4Y-4YFBDP-NMQA", "JKLD-9XLKM6-SU95", "BJ8B-8XCVEL-YWJN", "RQZG-YVHK3T-LBJ9", "8VAF-8QHLHL-PPG7", "VFQP-MUYUP9-EASC", "JCMA-6Q4ULF-3XH5", "5Q5K-Y9KLR5-YSB5", "CT8V-7WMLVQ-JUJF", "ZKKC-XKLBK9-BELA", "ML3E-7UBVGJ-6GVP", "4BZU-EQRCHT-7N42", "MMRX-24ESKQ-H8B5", "U6CR-DL8YDM-EGAL", "H4WL-C99EF9-94ZQ", "BS3M-QRW6P6-NYFU", "L7KW-Z66TLS-NTE9", "6EMC-ZJ6EZM-LFSD", "PUDR-QZD49H-TC6J", "DXJ2-G9S5EV-K87S", "G2NR-QE5R6A-4CT6", "CMHH-9KX5YA-PFF2", "BQXZ-5BUE5L-7MLU", "RLBQ-T4UX3A-RZUE", "RFDF-7BKW8K-NWHS", "CRFS-7YA7NY-WYHL", "NJCA-Q48D7N-9W25", "UHTZ-X9RAGX-FTWP", "U2PW-BVKPEA-H7Z9", "2DF4-86K6LZ-8HP2", "K8KG-ND95CV-9JLT", "GVHL-9BK36M-LGAA", "TLCE-JASJL7-HBST", "JDA6-HQB7VG-LETQ", "76H6-T4WWTJ-BSBG", "JDS2-XVM7LZ-83YX", "ULDV-GRHW5Z-F6NR", "5MZA-TQ656Q-SNUT", "8A24-WWCTQE-2R6P", "PZNY-7YEXP6-E3R8", "S5NL-FQ7X7Z-A2FG", "B7X4-RL84VW-3BZ9", "BYKD-YUKK7P-Z7Y2", "4UBM-77XRY6-BKDA", "3YPG-4KAVQV-YZ3A", "H96D-LY22KT-R94R", "L3BA-TLVK4M-QJUM", "4Q67-YFN4XE-RGFG", "KBAF-DA9T5G-UMG3", "W687-W39L8X-3PKU", "CYQQ-ZCVTUR-C5YV", "C9N6-FG6ZKW-899J", "BY2K-A7UYVC-7QX3", "M7WN-3RYT7E-8YTD", "PKH9-KA936R-NQGN", "226G-KNMKJZ-JA3T", "36V6-7XMDDV-6KH6", "LAKF-EG3KY2-5M3T", "BJBM-CLDKFX-65A3", "JRPN-ANARWG-89AX", "AZP5-NXBDYK-6KNU", "GW5H-HSE3SH-BRCG", "KCVP-2PZ4B8-LMP2", "QD8W-FEL2TN-NBZ9", "KR6L-P22VWV-2Y5N", "QTC7-LTGSVN-JKRH", "W9XH-9QCGFQ-XBED", "EWQU-2TGFGV-TRAU", "HQ5W-SWWJXL-ER2Y", "XYAX-A27AAF-VMYV", "WJ68-CU3WW4-DGNW", "NQJT-35QSKZ-ZGE3", "J929-YMQJZJ-62HR", "LTXQ-P5GAXP-WWTK", "CT5X-VP8YBA-NXX8", "DMSV-3UPB9L-EDLV", "UNXK-S7J8HK-SXYX", "G6SE-5ND8ST-D9YJ", "NPLH-9ZGQME-37X5", "P6YX-5WRX2F-8CP5", "XT9G-WKPBCK-92PW", "7PCS-RBYAC7-89R7", "2R68-58G4KX-S85T", "H5SN-YGASFD-F2WR", "YBQ4-SCNPQQ-T6TJ", "SXCU-ZSZ7CK-A86F", "3RN7-P67YHM-XRGD", "NBP6-GWPTU3-5TU5", "6S85-LPXD3Q-8NUB", "4D99-ZPKBNF-GQQJ", "F9TX-S5ZSM5-JEV2", "9ZF9-FC8F8Z-R74R", "Z9HQ-8DS839-DWZ6", "LCD9-E7L2RK-Z7MM", "JKL9-GGDFFG-8SNC", "NXE6-7MN9Q3-2Q6F", "KU9D-EVXZWC-XG4N", "JS2P-G5SQ37-DS2X", "JD2N-T45UQ9-MML4", "YQU9-FDMULX-FLTN", "SF82-JUCASR-TFPP", "E7QF-27QVJA-235N", "CTLW-SU2KVB-HFMF", "HZKT-N6QA5H-BFQH", "YBDW-M58DNG-VXRR", "JLLP-P6N623-6CEQ", "988X-ZLRMQE-PWT8", "DT5C-27KDFU-9V5J", "WQM4-G2AHHQ-T9FG", "QHYK-U98HRD-Z4TU"]
+
+var divID = ["gift-card-left", "gift-card-center", "gift-card-right"];
+var previousCardIdx = -1;
 $(window).on('load', function() {
 
   console.log("window-loaded");
 
   $(
     "<div id=\"gift-card-container\">" +
-    "<div id=\"gift-card-left\" class=\"button\"> left </div>" +
-    "<div id=\"gift-card-center\" class=\"button\"> center</div>" +
-    "<div id=\"gift-card-right\" class=\"button\"> right </div>" +
+    "<div id=\"" + divID[0] + "\" class=\"button\"> left </div>" +
+    "<div id=\"" + divID[1] + "\" class=\"button\"> center</div>" +
+    "<div id=\"" + divID[2] + "\" class=\"button\"> right </div>" +
     "</div>"
   ).insertBefore("#gc-redemption-form-heading");
 
   //$("<div id=\"button-fill-card\" class=\"button\">Test</div>").insertBefore("#gc-redemption-form-heading");
 
-  $('#gift-card-left').click(function() {
+  $('#' + divID[0]).click(function() {
     console.log("button-fill-card click");
-    $('#gc-redemption-input').val("button-fill-card click");
+    var card = $(this).text();
+    console.log("button-fill-card card -> " + card);
+    $('#gc-redemption-input').val(card);
+    var saved = previousCardIdx == -1 ? 0 : previousCardIdx;
+    chrome.storage.sync.set({
+      "previousCardIdx": saved
+    });
+  });
+
+  $('#' + divID[1]).click(function() {
+    console.log("button-fill-card click");
+    var card = $(this).text();
+    console.log("button-fill-card card -> " + card);
+    $('#gc-redemption-input').val(card);
+    var saved = previousCardIdx == -1 ? 1 : previousCardIdx + 1;
+    chrome.storage.sync.set({
+      "previousCardIdx": saved
+    });
+  });
+
+  $('#' + divID[2]).click(function() {
+    console.log("button-fill-card click");
+    var card = $(this).text();
+    console.log("button-fill-card card -> " + card);
+    $('#gc-redemption-input').val(card);
+    var saved = previousCardIdx == -1 ? 2 : previousCardIdx + 2;
+    chrome.storage.sync.set({
+      "previousCardIdx": saved
+    });
+  });
+
+  chrome.storage.sync.get('previousCardIdx', function(result) {
+    previousCardIdx = -1;
+    if (result.previousCardIdx != undefined) {
+      previousCardIdx = parseInt(result.previousCardIdx);
+    }
+
+    if (previousCardIdx == -1) {
+      $('#' + divID[0]).text(listCard[0]);
+      $('#' + divID[0]).addClass("button-availabel");
+      $('#' + divID[1]).text(listCard[1]);
+      $('#' + divID[1]).addClass("button-availabel");
+      $('#' + divID[2]).text(listCard[2]);
+      $('#' + divID[2]).addClass("button-availabel");
+    } else if (previousCardIdx >= 0) {
+      $('#' + divID[0]).text(listCard[previousCardIdx]);
+      $('#' + divID[0]).addClass("button-redeemed");
+      $('#' + divID[1]).text(listCard[previousCardIdx + 1]);
+      $('#' + divID[1]).addClass("button-availabel");
+      $('#' + divID[2]).text(listCard[previousCardIdx + 2]);
+      $('#' + divID[2]).addClass("button-availabel");
+    }
   });
 
 });
--- popup.js
+++ popup.js
@@ -1,32 +1,8 @@
-
 $(function() {
-
-  chrome.storage.sync.get('total', function(result){
-    $('#spend').text(result.total);
-  });
-
-  $('#submitAmount').click(function(){
+  $('#submitAmount').click(function() {
     console.log('submitAmount click');
-
-    chrome.storage.sync.get('total', function(budget) {
-      var newTotal = 0;
-      if (budget.total) {
-        newTotal += parseInt(budget.total);
-      }
-      console.log('newTotal ' + newTotal);
-
-      var amount = $('#amount').val();
-      if (amount) {
-        newTotal += parseInt(amount);
-      }
-      console.log('newTotal ' + newTotal);
-
-      chrome.storage.sync.set({
-        'total': newTotal
-      });
-      $('#spend').text(newTotal);
-      $('#amount').val("");
+    chrome.storage.sync.set({
+      'previousCardIdx': -1
     });
-
   });
 });
